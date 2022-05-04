d83a275bb44818ad7d2ea638c9cc90012f05d918
Obioma <justobioma@outlook.com>
2022-05-04 20:03:51 +0100

    Committing files to repo

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x13F279a3abD33D1b463DB8A1B8279844f04Ea883";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
